16afda18e7d21e198cda8575cbc7faf4f3e9bcf0
sam <SamAng219255@gmail.com>
2018-03-06 13:57:00 -0800

    updated

--- loginform.php
+++ loginform.php
@@ -1,22 +1,36 @@
 <?php if($_SERVER['PHP_SELF']!='/chat/index.php') {echo '<meta http-equiv="refresh" content="0; URL=./?page=3">';};?>
 
+<?php
+	if(isset($_GET['error'])) {
+		echo '<div id="errorbox">';
+		if($_GET['error']==0) {
+			echo 'Passwords do not match.';
+		}
+		elseif($_GET['error']==1) {
+			echo 'Username or Password missing.';
+		}
+		elseif($_GET['error']==2) {
+			echo 'Username is taken.';
+		}
+		echo '</div>';
+	}
+?>
+
 <div id="cphold">
 <div id="lcp" class="cp"><form class="loginform" action="./?page=3" method="post">
 	Username:<br>
-	<input type="text" name="username"><br>
+	<input type="text" name="username" required><br>
 	Password:<br>
-	<input type="password" name="password"><br>
-	<input type="hidden" name="status" value="verification"><br>
+	<input type="password" name="password" required><br>
 	<input type="submit" value="Login"><br>
 </form></div>
 <div id="rcp" class="cp"><form class="loginform" action="./?page=4" method="post">
         Username:<br>
-        <input type="text" name="username"><br>
+        <input type="text" name="username" required><br>
         Password:<br>
-        <input type="password" name="password"><br>
+        <input type="password" name="password" required><br>
         Retype Password:<br>
-        <input type="password" name="password"><br>
-        <input type="hidden" name="status" value="verification"><br>
+        <input type="password" name="password2" required><br>
         <input type="submit" value="Register"><br>
 </form></div>
 </div>
--- register.php
+++ register.php
@@ -1,17 +1,28 @@
 <?php if($_SERVER['PHP_SELF']!='/chat/index.php') {echo '<meta http-equiv="refresh" content="0; URL=./?page=4">';};?>
 
 <?php
-	if(isset($_POST['username']) && isset($_POST['password'])) {
-		$hashed=password_hash($_POST['password'],PASSWORD_DEFAULT);
-		$servername="locahost";
-		$username="chatter";
-		$password="GeArᛈᚨᛊᚹᚱᛥ";
-		$conn = new PDO("mysql:host=localhost;dbname=chat",$username,$password);
-		$sql="INSERT INTO users (id, username, password, quirks) VALUES (0,'".$_POST['username']."','".$hashed."','E')";
-		$conn->exec($sql);
-		echo 'You have successfully created the account: '.$_POST['username'].'.';
+	if($_POST['username']!='' && $_POST['password']!='' && $_POST['password2']!='') {
+		if($_POST['password']==$_POST['password2']) {
+			$hashed=password_hash($_POST['password'],PASSWORD_DEFAULT);
+			$servername="127.0.0.1";
+			$username="chatter";
+			$password="GeArᛈᚨᛊᚹᚱᛥ";
+			$conn = mysqli_connect($servername, $username, $password);
+			if(!mysqli_query($conn,"SELECT `username` from `chat`.`users` where username='".$_POST['username']."'")) {
+				$sql="INSERT INTO `chat`.`users` (`id`, `username`, `password`, `quirks`) VALUES (0,'".$_POST['username']."','".$hashed."','E')";
+				mysqli_query($conn,$sql);
+				echo 'You have successfully created the account: '.$_POST['username'].'.<br>';
+				//echo $sql;
+			}
+			else {
+				echo '<meta http-equiv="refresh" content="0; URL=./?page=1&error=2">';
+			}
+		}
+		else {
+			echo '<meta http-equiv="refresh" content="0; URL=./?page=1&error=0">';
+		}
 	}
 	else {
-		echo '<meta http-equiv="refresh" content="0; URL=./?page=1">';
+		echo '<meta http-equiv="refresh" content="0; URL=./?page=1&error=1">';
 	}
 ?>
