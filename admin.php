be3da8cf7dc406c1a4610bba19613d38db54d0b3
SamAng219255 <SamAng219255@gmail.com>
2018-07-28 19:10:14 -0700

    added buttons to admin page

--- admin.php
+++ admin.php
@@ -15,14 +15,24 @@ echo '<div id="settingbox">
 </div>';
 
 }
-else {
-if($_GET['place']=='users') {
+elseif($_GET['place']=='users') {
 	$foo=['ASC','ASC','ASC','ASC','ASC','ASC'];
+	$fooArrow=['','','','','',''];
 	$bar=['id','username','permissions','active','laston','ip'];
 	if(isset($_GET["sort"])) {
 		$temp=explode(".",$_GET["sort"]);
 		$usersquery="SELECT `id`,`username`,`ip`,`laston`,`active`,`permissions` FROM `chat`.`users` ORDER BY ".$temp[0]." ".$temp[1].";";
-		$foo[array_search($temp[0],$bar)]='DESC';
+		$sortDirec='DESC';
+		if($temp[1]=='DESC') {
+			$sortDirec='ASC';
+		}
+		$foo[array_search($temp[0],$bar)]=$sortDirec;
+		if($sortDirec=='DESC') {
+			$fooArrow[array_search($temp[0],$bar)]=' ↓';
+		}
+		else {
+			$fooArrow[array_search($temp[0],$bar)]=' ↑';
+		}
 	}
 	else {
 		$usersquery="SELECT `id`,`username`,`ip`,`laston`,`active`,`permissions` FROM `chat`.`users`;";
@@ -31,12 +41,16 @@ if($_GET['place']=='users') {
 	$time=date("Y-m-d H:i:s");
 	$timewas=date("Y-m-d H:i:s",time()-1);
 		echo '<table class="admintable"><tr>
-			<th onclick="window.location=\'./?p=admin&place=users&sort=id.'.$foo[0].'\'">Id</th>
-			<th onclick="window.location=\'./?p=admin&place=users&sort=username.'.$foo[1].'\'">Username</th>
-			<th onclick="window.location=\'./?p=admin&place=users&sort=permissions.'.$foo[2].'\'">Permission</th>
-			<th onclick="window.location=\'./?p=admin&place=users&sort=active.'.$foo[3].'\'">Place</th>
-			<th onclick="window.location=\'./?p=admin&place=users&sort=laston.'.$foo[4].'\'">Last On</th>
-			<th onclick="window.location=\'./?p=admin&place=users&sort=ip.'.$foo[5].'\'">IP</th>
+			<th class="sortable" onclick="window.location=\'./?p=admin&place=users&sort=id.'.$foo[0].'\'">Id'.$fooArrow[0].'</th>
+			<th class="sortable" onclick="window.location=\'./?p=admin&place=users&sort=username.'.$foo[1].'\'">Username'.$fooArrow[1].'</th>
+			<th class="sortable" onclick="window.location=\'./?p=admin&place=users&sort=permissions.'.$foo[2].'\'">Permission'.$fooArrow[2].'</th>
+			<th class="sortable" onclick="window.location=\'./?p=admin&place=users&sort=active.'.$foo[3].'\'">Place'.$fooArrow[3].'</th>
+			<th class="sortable" onclick="window.location=\'./?p=admin&place=users&sort=laston.'.$foo[4].'\'">Last On'.$fooArrow[4].'</th>
+			<th class="sortable" onclick="window.location=\'./?p=admin&place=users&sort=ip.'.$foo[5].'\'">IP'.$fooArrow[5].'</th>
+			<th>Delete</th>
+			<th>Ban</th>
+			<th>Reset Password</th>
+			<th>Alert</th>
 		</tr>';
 	for($i=0; $i<$usersqueryresult->num_rows; $i++) {
 		$row=mysqli_fetch_row($usersqueryresult);
@@ -44,11 +58,49 @@ if($_GET['place']=='users') {
 		if($row[3]!=$time && $row[3]!=$timewas) {
 			$place="-1";
 		}
-		echo '<tr><td>'.$row[0].'</td><td>'.$row[1].'</td><td>'.$row[5].'</td><td>'.$place.'</td><td>'.$row[3].'</td><td>'.$row[2].'</td></tr>';
+		echo '
+		<tr>
+			<td>'.$row[0].'</td>
+			<td>'.$row[1].'</td>
+			<td>'.$row[5].'</td>
+			<td>'.$place.'</td>
+			<td>'.$row[3].'</td>
+			<td>'.$row[2].'</td>
+			<td><div class="button" onclick="sure(\''.$row[1].'\',\'delete\')">Delete</div></td>
+			<td><div class="button" onclick="sure(\''.$row[1].'\',\'ban\')">Ban</div></td>
+			<td><div class="button" onclick="sure(\''.$row[1].'\',\'reset the password of\')">Reset Password</div></td>
+			<td><input onkeypress="if(event.which==13||event.keyCode==13) {  }" type="text"></td>
+		</tr>';
 	}
-	echo "</table>";
+	echo '
+	</table>
+	<script>
+		function sure(user,action) {
+			randText=Math.ceil(Math.random()*Math.pow(36,11)).toString(36).toUpperCase();
+			sureTest=prompt("Are you sure you want to "+action+" "+user+"?\nIf so type the username")
+			if(sureTest==user) {
+				if(action=="delete") {
+					//delete
+					alert(user+" has been deleted.");
+				}
+				else if(action=="ban") {
+					//ban
+					alert(user+" has been banned.");
+				}
+				else if(action=="reset the password of") {
+					//set password to randText
+					alert(user+"\'s new password is "+randText);
+				}
+				location.reload();
+			}
+			else if(sureTest!=null) {
+				alert("Incorrect Name")
+			}
+		}
+	</script>
+';
 }
-}}
+}
 else {
   echo '<meta http-equiv="refresh" content="0; URL=./?p=home">';
 }
