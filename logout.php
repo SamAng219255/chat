a84f1b3263387e4fb6e270d6b60b7c394894fa01
SamAng219255 <SamAng219255@gmail.com>
2018-11-18 15:15:02 -0800

    updated

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php $fooip=explode("/",$_SERVER['PHP_SELF']); if($fooip[count($fooip)-1]!='index.php') {echo '<meta http-equiv="refresh" content="0; URL=./?page=5">';};?>
 
 <?php
-$_SESSION['loggedin']='no';
-$_SEESION['username']='Not Signed In';
+session_unset();
+session_destroy();
 echo '<meta http-equiv="refresh" content="0; URL=./?p=login&target='.$_GET["target"].'">';
 ?>
